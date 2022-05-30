53270c73d643feb5b8c360d9b7516f7aaedda005
Anicod <animeshraj735@gmail.com>
2022-05-31 02:42:54 +0530

    added: float string and intiger array

---
+++ src/Maxnumber.java
@@ -0,0 +1,10 @@
+public class Maxnumber {
+    public static void main(String[] args) {
+        Integer array[] = {3, 8, 10};
+        Float array1[] = {1.2f, 2.1f, 3.2f};
+        String array2[] = {"animesh", "harsh", "navneet"};
+
+    }
+}
+
+
